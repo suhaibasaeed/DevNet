ca2272fc68f95424a69f99524283a509b145b504
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-02-08 20:48:58 +0000

    Created two more test cases

--- Unit Tests/test_example.py
+++ Unit Tests/test_example.py
@@ -11,6 +11,14 @@ class code_test(unittest.TestCase):
     def test_circumference(self): # This is the test case
         # If the methods 1st and 2nd parameter values are equal the assertion will pass
         self.assertEqual(calculate_circumference(5),31.41592653589793) # i.e. if function returns 31.4592...
+    # Test edge case of circle with radius of 0
+    def test_circumference_zero(self):
+        actual = calculate_circumference(0)
+        self.assertEqual(actual,0)
+
+    # Test if function correctly raises error if non-number passed in as argument
+    def test_false_circumference(self):
+        self.assertRaises(calculate_circumference("Five"))
 
 # Call function to run the testcases
 unittest.main() # This is the test runner
