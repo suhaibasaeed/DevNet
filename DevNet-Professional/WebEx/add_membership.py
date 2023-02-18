a617eddfa926753d298e45bd688ea9f2ff42fe00
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-02-18 21:14:09 +0000

    Added delete and memberships code

---
+++ DevNet-Professional/WebEx/add_membership.py
@@ -0,0 +1,36 @@
+import requests
+from json import dumps
+
+# Get your token here after logging in: https://developer.webex.com/docs/api/getting-started
+token = "Njg1ODgzNjUtYzM1OC00ZmZmLTg2MDEtYzczY2FmNWI1MGNhYzE1M2FlZWQtNDRk_P0A1_4e881e32-f617-4e88-9ba1-0d4fe8ea601d"
+
+# URL we're hitting to hit rooms
+url = "https://webexapis.com/v1/rooms"
+# Headers inc credentials in form of bearer token
+headers = {"Authorization": f"Bearer {token}"}
+
+# GET request so we can verify the team was actually created
+get_response = requests.get(url, headers=headers).json()
+
+# Loop through rooms and get ID of the ones with suhaib in the title
+for room in get_response["items"]:
+    if 'Marketing' in room["title"] or 'Random' in room["title"]:
+        room_id = room["id"]
+        
+        # URL we're hitting for memberships
+        membership_url = "https://webexapis.com/v1/memberships"
+        # Headers and body for the POST request
+        membership_headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+        # Person email is what we got when we created the bot
+        membership_body = {"roomId": room_id, "personEmail": "cbtbot@webex.bot", "isModerator": False}
+
+        # POST request to add the bot to the room
+        post_response = requests.post(membership_url, headers=membership_headers, data=dumps(membership_body))
+
+        # Raise errors
+        post_response.raise_for_status()
+
+        if post_response.status_code == 200:
+            print(f"Bot added successfully to room {room['title']}")
+
+        
---
+++ DevNet-Professional/WebEx/delete_rooms.py
@@ -0,0 +1,29 @@
+import requests
+from json import dumps
+
+# Get your token here after logging in: https://developer.webex.com/docs/api/getting-started
+token = "Njg1ODgzNjUtYzM1OC00ZmZmLTg2MDEtYzczY2FmNWI1MGNhYzE1M2FlZWQtNDRk_P0A1_4e881e32-f617-4e88-9ba1-0d4fe8ea601d"
+
+# URL we're hitting to hit rooms
+url = "https://webexapis.com/v1/rooms"
+# Headers inc credentials in form of bearer token
+headers = {"Authorization": f"Bearer {token}"}
+
+# GET request so we can verify the team was actually created
+get_response = requests.get(url, headers=headers).json()
+
+# Loop through rooms and get ID of the ones with suhaib in the title
+for room in get_response["items"]:
+    if 'suhaib' in room["title"]:
+        room_id = room["id"]
+        
+        # New room URL
+        room_url = f"{url}/{room_id}"
+        # DELETE request to delete the room
+        delete_response = requests.delete(room_url, headers=headers)
+    
+        # Raise errors
+        delete_response.raise_for_status()
+        
+        if delete_response.status_code == 204:
+            print(f"Room {room['title']} was successfully deleted")
