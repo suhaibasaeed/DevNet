65f01aad08db2f8ac0b7f41a03af714d75aea9c9
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-03-01 20:59:34 +0000

    added dna-c auth script

---
+++ DevNet-Professional/DNA-C/auth.py
@@ -0,0 +1,13 @@
+import requests
+
+base_url = "https://sandboxdnac.cisco.com/dna/"
+auth_endpoint = "system/api/v1/auth/token"
+
+user = "devnetuser"
+password = "Cisco123!"
+# POST to get token
+auth_response = requests.post(
+    url=f"{base_url}{auth_endpoint}", auth=(user, password), verify=False
+).json()
+# Parse out token
+token = auth_response["Token"]
