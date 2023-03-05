f15d0bea3479619781e2d8af6be33835c1263a83
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-03-05 20:00:57 +0000

    Added ospf filter to subtree

--- DevNet-Professional/Scrapli-yang/nc-subtree.py
+++ DevNet-Professional/Scrapli-yang/nc-subtree.py
@@ -1,6 +1,4 @@
 from scrapli_netconf.driver import NetconfDriver
-import xmltodict
-from rich.pretty import pprint
 
 device = {
     "host": "192.168.1.108",
@@ -14,6 +12,21 @@ device = {
 # Hierarchy to filter on
 ospf_filter = """
 <ospf-oper-data xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-ospf-oper">
+  <ospf-state>
+    <ospf-instance>
+      <af>address-family-ipv4</af>
+      <router-id>167837953</router-id>
+        <ospf-area>
+            <area-id>0</area-id>
+            <ospf-interface>
+                <name>GigabitEthernet2</name>
+                  <ospf-neighbor>
+                    <neighbor-id>10.1.1.2</neighbor-id>
+                  </ospf-neighbor>
+            </ospf-interface>
+        </ospf-area>
+    </ospf-instance>
+  </ospf-state>
 </ospf-oper-data>
 """
 # Open connection to device
@@ -22,7 +35,4 @@ with NetconfDriver(**device) as conn:
     # Pass in filter and filter type
     response = conn.get(filter_=ospf_filter, filter_type="subtree")
 
-    # Convert XML to Python dictionary
-    ospf_dict = xmltodict.parse(response.result)
-
-    pprint(ospf_dict)
+    print(response.result)
